1fbd440e95ead2b18209e7bc90cfd99073c1d43b
Juan Carlos Otero <50993667+juancarlosotero@users.noreply.github.com>
2019-06-21 15:26:32 -0500

    Taller2_Problema1

---
+++ Taller2_problema1/Main.java
@@ -0,0 +1,13 @@
+package Taller2_problema1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        problema1 correrprograma = new problema1();
+        correrprograma.ejecutar();
+
+    }
+
+}
